f083766b58672c60b9f524b117120e5d6b09c113
Paul <paulsegfault@gmail.com>
2020-12-06 01:07:48 +0200

    Fixed device name

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ timedatectl set-ntp true
 cryptsetup open /dev/nvme0n1p2 cryptroot
 mount /dev/mapper/cryptroot /mnt
 mkdir /mnt/boot
-mount /dev/nvm0n1p1 /mnt/efi
-cryptsetup open /dev/nvm0n1p3 crypthome
+mount /dev/nvme0n1p1 /mnt/efi
+cryptsetup open /dev/nvme0n1p3 crypthome
 mkdir /mnt/home
 mount /dev/mapper/crypthome /mnt/home
