dc58abfdfede19d04dd99cdcd3a01add035ea976
Paul <paulsegfault@gmail.com>
2020-12-06 01:31:49 +0200

    Fixed mounting

--- install.sh
+++ install.sh
@@ -4,18 +4,20 @@ if [[ -z /dev/mapper/cryptroot ]]; then
     cryptsetup open /dev/nvme0n1p2 cryptroot
 fi
 
-mount /dev/mapper/cryptroot /mnt
-
-if [[ -z /mnt/boot ]]; then
-    mkdir -p /mnt/boot
-    mount /dev/nvme0n1p1 /mnt/efi
+if [[ -z "$(ls -A /mnt)" ]]; then
+    mount /dev/mapper/cryptroot /mnt
+    
+    if [[ -z /mnt/boot ]]; then
+        mkdir -p /mnt/boot
+        mount /dev/nvme0n1p1 /mnt/efi
+    fi
+    
+    if [[ -z /dev/mapper/crypthome ]]; then
+        cryptsetup open /dev/nvme0n1p3 crypthome
+    fi
+    if [[ -z /mnt/home ]]; then
+        mkdir -p /mnt/home
+        mount /dev/mapper/crypthome /mnt/home
+    fi
+    
 fi
-
-if [[ -z /dev/mapper/crypthome ]]; then
-    cryptsetup open /dev/nvme0n1p3 crypthome
-fi
-if [[ -z /mnt/home ]]; then
-    mkdir -p /mnt/home
-    mount /dev/mapper/crypthome /mnt/home
-fi
-
