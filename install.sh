3a8423700090b4f25840a4c221381796044c678f
Paul <paulsegfault@gmail.com>
2020-12-06 01:17:30 +0200

    More fixes

--- install.sh
+++ install.sh
@@ -3,11 +3,19 @@ timedatectl set-ntp true
 if [[ -z /dev/mapper/cryptroot ]]; then
     cryptsetup open /dev/nvme0n1p2 cryptroot
 fi
+
 mount /dev/mapper/cryptroot /mnt
-mkdir -p /mnt/boot
-mount /dev/nvme0n1p1 /mnt/efi
+
+if [[ -z /mnt/boot ]]; then
+    mkdir -p /mnt/boot
+    mount /dev/nvme0n1p1 /mnt/efi
+fi
+
 if [[ -z /dev/mapper/crypthome ]]; then
     cryptsetup open /dev/nvme0n1p3 crypthome
 fi
-mkdir -p /mnt/home
-mount /dev/mapper/crypthome /mnt/home
+if [[ -z /mnt/home ]]; then
+    mkdir -p /mnt/home
+    mount /dev/mapper/crypthome /mnt/home
+fi
+
