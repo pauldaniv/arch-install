eed8a82044d8840f3cc679204c2b539bb84705b0
Paul <paulsegfault@gmail.com>
2020-12-07 23:17:17 +0200

    Added some more commands to chrooted env

--- chroot.sh
+++ chroot.sh
@@ -16,4 +16,29 @@ echo KEYMAP=dvorak >> /etc/vconsole.conf
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
 echo paulsrv >> /etc/hostname
 
-pacmat -S grub efibootmgr vim git networkmanager mtools dosfstools os-prober
+pacmat -S grub efibootmgr vim git networkmanager mtools dosfstools os-prober linux-headers xdg-utils xdg-user-dirs dialog base-devel mesa intel-ucode sudo
+
+useradd -mG wheel paul
+
+passwd paul
+passwd
+# edit visudo
+
+# vim mkinitcpio.conf
+#add hooks -- HOOKS(base udev autodetect keyboard keymap modconf block encrypt keyboard fcsk)
+# mkinitcpio -p linux
+# grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+# grub-mkconfig -o /boot/grub/grub.cfg
+#if using encryption, enable grub cmdline 
+# blkid -s UUID -o value /dev/cryptsetup/cryptroot >> /etc/default/grub
+#add line "cryptdevice=UUID=<UUID>:cryptroot root=/dev/mapper/cryptroot"
+# grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
+
+
+
+
+
+
